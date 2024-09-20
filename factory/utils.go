7c7b389811e22dbb410b9256ca63ce1f00e1623d
Edvinas Kručas <edv.krucas@gmail.com>
2024-09-20 12:36:40 +0300

    Added always true evaluator

--- factory/utils.go
+++ factory/utils.go
@@ -1,6 +1,9 @@
 package factory
 
-import "context"
+import (
+	"context"
+	"github.com/mbvienasbaitas/go-manager"
+)
 
 type FuncEvaluator[T any] func(ctx context.Context, name string) bool
 
@@ -12,4 +15,10 @@ type FuncBuilder[T any] func(ctx context.Context, name string) (T, error)
 
 func (receiver FuncBuilder[T]) Build(ctx context.Context, name string) (T, error) {
 	return receiver(ctx, name)
+}
+
+func AlwaysTrueEvaluator[T any]() manager.Evaluator[T] {
+	return FuncEvaluator[T](func(_ context.Context, _ string) bool {
+		return true
+	})
 }
